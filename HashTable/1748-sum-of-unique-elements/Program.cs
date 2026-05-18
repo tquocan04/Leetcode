9aec1c0c64f12b984fea801556bb5e2c059a7303
Ấn Trần <tquocan04@gmail.com>
2026-05-19 03:04:50 +0700

    1748-Sum-of-Unique-Elements

---
+++ HashTable/1748-sum-of-unique-elements/Program.cs
@@ -0,0 +1,38 @@
+﻿class Solution
+{
+    // Time Complexity: O(n)
+    // Space Complexity: O(1) - using Dictionary
+
+    public int SumOfUnique(int[] nums)
+    {
+        int result = 0;
+        Dictionary<int, int> dict = [];
+
+        foreach(var x in nums)
+        {
+            dict[x] = dict.GetValueOrDefault(x) + 1;
+            
+            if (dict[x] == 1)
+                result += x;
+            else if (dict[x] == 2)
+                result -= x;
+        }
+        
+        return result;    
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int [] nums = [.. input.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(s => s.Trim())
+                                    .Select(s => int.Parse(s))];
+            
+            Solution sln = new ();
+            Console.WriteLine($"{sln.SumOfUnique(nums)}");
+        }
+    }
+}
