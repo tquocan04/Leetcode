b5f5e8ab3ad0cf37acefeafc8bf2eed65b890922
tquocan04 <tquocan04@gmail.com>
2026-06-08 22:00:41 +0700

    206-Reverse-Linked-List

---
+++ LinkedList/206-Reverse-Linked-List/Program.cs
@@ -0,0 +1,29 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    // Iterative method
+    public ListNode ReverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (current != null)
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    // Recursive method
+}
