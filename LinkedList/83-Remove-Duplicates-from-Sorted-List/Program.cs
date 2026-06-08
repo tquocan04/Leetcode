feedb53954071c8742b10d9ddf451bd82fddecf9
tquocan04 <tquocan04@gmail.com>
2026-06-08 14:08:50 +0700

    83-Remove-Duplicates-from-Sorted-List

---
+++ LinkedList/83-Remove-Duplicates-from-Sorted-List/Program.cs
@@ -0,0 +1,30 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode dummy = new (-101);
+        ListNode current = dummy;
+
+        while (head != null)
+        {
+            if (current.val != head.val)
+            {
+                current.next = head;
+                current = head;
+            }
+            
+            head = head.next;
+        }
+        
+        current.next = null;
+
+        return dummy.next;
+    }
+}
