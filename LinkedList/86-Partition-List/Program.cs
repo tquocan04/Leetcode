101be2b76e52d43ff983d867b6ba6a851413f5a2
tquocan04 <tquocan04@gmail.com>
2026-06-11 15:33:33 +0700

    86-Partition-List

---
+++ LinkedList/86-Partition-List/Program.cs
@@ -0,0 +1,35 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode Partition(ListNode head, int x) {
+        ListNode dummy = new (0);
+        ListNode dummy2 = new (0);
+        
+        ListNode less = dummy;
+        ListNode greater = dummy2;
+        
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = head;
+            }
+            else {
+                greater.next = head;
+                greater = head;
+            }
+            head = head.next;
+        }
+        greater.next = null;
+
+        less.next = dummy2.next;
+
+        return dummy.next;
+    }
+}
